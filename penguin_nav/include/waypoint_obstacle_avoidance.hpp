5e228a62607b0292e3b0b13d9bc824e881acb8c6
nonanonno <as.nonanonno@gmail.com>
2024-11-29 13:25:42 +0000

    feat: add cut_behind

--- penguin_nav/include/waypoint_obstacle_avoidance.hpp
+++ penguin_nav/include/waypoint_obstacle_avoidance.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <nav_msgs/msg/detail/occupancy_grid__struct.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <penguin_nav_msgs/srv/adjust_waypoints.hpp>
@@ -19,11 +20,18 @@ private:
   void
   global_costmap_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
 
+  void pose_callback(
+      const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
+
   rclcpp::Service<penguin_nav_msgs::srv::AdjustWaypoints>::SharedPtr
       adjust_waypoints_service_;
   rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr
       global_costmap_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr
+      pose_sub_;
 
   nav_msgs::msg::OccupancyGrid::ConstSharedPtr global_costmap_;
+  geometry_msgs::msg::PoseWithCovarianceStamped::ConstSharedPtr pose_;
 };
+
 } // namespace penguin_nav
--- penguin_nav/src/waypoint_obstacle_avoidance.cpp
+++ penguin_nav/src/waypoint_obstacle_avoidance.cpp
@@ -10,6 +10,7 @@ namespace penguin_nav {
 
 using geometry_msgs::msg::Point;
 using geometry_msgs::msg::Pose;
+using geometry_msgs::msg::PoseStamped;
 using nav_msgs::msg::OccupancyGrid;
 
 namespace {
@@ -184,6 +185,107 @@ TEST_CASE("testing adjust_lateral") {
     auto result = adjust_lateral(map, p, 1, 1, 0.1);
     test(result, {make_pose(6.0, 5, M_PI_2), true});
   }
+  SUBCASE("cannot adjust") {
+    auto p = make_pose(5, 5, 0);
+    auto result = adjust_lateral(map, p, 0.9, 0.9, 0.1);
+    test(result, {p, false});
+  }
+}
+
+std::vector<PoseStamped> cut_behind(const std::vector<PoseStamped> &pts,
+                                    const Point &point) {
+  size_t i = 0;
+  for (; i < pts.size() - 1; i++) {
+    tf2::Vector3 p0, p1, e;
+    into(pts[i].pose.position, p0);
+    into(pts[i + 1].pose.position, p1);
+    into(point, e);
+
+    auto v0 = p1 - p0;
+    auto v1 = e - p0;
+    auto dot = v0.dot(v1);
+    if (dot < 0) {
+      break;
+    }
+  }
+
+  std::vector<PoseStamped> result;
+  for (size_t j = i; j < pts.size(); j++) {
+    result.push_back(pts[j]);
+  }
+
+  return result;
+}
+
+TEST_CASE("testing cut_behind") {
+  auto make_pose = [](double x, double y) {
+    PoseStamped pose;
+    pose.pose.position.x = x;
+    pose.pose.position.y = y;
+    return pose;
+  };
+  auto make_point = [](double x, double y) {
+    Point p;
+    p.x = x;
+    p.y = y;
+    return p;
+  };
+  auto test = [](const std::vector<PoseStamped> &ans,
+                 const std::vector<PoseStamped> &exp) {
+    CHECK(ans.size() == exp.size());
+    for (size_t i = 0; i < ans.size(); i++) {
+      CHECK(ans[i].pose.position.x == exp[i].pose.position.x);
+      CHECK(ans[i].pose.position.y == exp[i].pose.position.y);
+    }
+  };
+
+  SUBCASE("x-straight") {
+    std::vector<PoseStamped> pts = {
+        make_pose(0, 0), make_pose(1, 0), make_pose(2, 0),
+        make_pose(3, 0), make_pose(4, 0), make_pose(5, 0),
+    };
+    SUBCASE("inside") {
+      auto result = cut_behind(pts, make_point(2.5, 0));
+      test(result, {pts[3], pts[4], pts[5]});
+    }
+    SUBCASE("before") {
+      auto result = cut_behind(pts, make_point(-1, 0));
+      test(result, pts);
+    }
+    SUBCASE("after") {
+      auto result = cut_behind(pts, make_point(6, 0));
+      test(result, {pts[5]}); // should have last point
+    }
+  }
+
+  SUBCASE("y-straight") {
+    std::vector<PoseStamped> pts = {
+        make_pose(0, 0), make_pose(0, 1), make_pose(0, 2),
+        make_pose(0, 3), make_pose(0, 4), make_pose(0, 5),
+    };
+    SUBCASE("inside") {
+      auto result = cut_behind(pts, make_point(0, 2.5));
+      test(result, {pts[3], pts[4], pts[5]});
+    }
+    SUBCASE("before") {
+      auto result = cut_behind(pts, make_point(0, -1));
+      test(result, pts);
+    }
+    SUBCASE("after") {
+      auto result = cut_behind(pts, make_point(0, 6));
+      test(result, {pts[5]}); // should have last point
+    }
+  }
+
+  SUBCASE("u-turn") {
+    std::vector<PoseStamped> pts = {make_pose(0, 0), make_pose(1, 0),
+                                    make_pose(2, 1), make_pose(1, 2),
+                                    make_pose(0, 2)};
+    SUBCASE("inside") {
+      auto result = cut_behind(pts, make_point(0.5, 0.4));
+      test(result, {pts[1], pts[2], pts[3], pts[4]});
+    }
+  }
 }
 
 } // namespace
@@ -199,6 +301,12 @@ WaypointObstacleAvoidanceNode::WaypointObstacleAvoidanceNode()
       "global_costmap/costmap", 10,
       std::bind(&WaypointObstacleAvoidanceNode::global_costmap_callback, this,
                 std::placeholders::_1));
+
+  pose_sub_ =
+      this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+          "pcl_pose", 10,
+          std::bind(&WaypointObstacleAvoidanceNode::pose_callback, this,
+                    std::placeholders::_1));
 }
 
 void WaypointObstacleAvoidanceNode::adjust_waypoints_callback(
@@ -207,15 +315,39 @@ void WaypointObstacleAvoidanceNode::adjust_waypoints_callback(
     std::shared_ptr<penguin_nav_msgs::srv::AdjustWaypoints::Response>
         response) {
   RCLCPP_INFO(this->get_logger(), "Received request to adjust waypoints");
-  response->modified = true;
+  response->modified = false;
+  response->waypoints = request->waypoints;
+
+  if (!global_costmap_) {
+    return;
+  }
+
+  for (size_t i = 0; i < request->waypoints.size(); i++) {
+    const auto &p = request->waypoints[i].pose;
+    const auto left_torelance = request->left_torelances[i];
+    const auto right_torelance = request->right_torelances[i];
+
+    auto ret = adjust_lateral(*global_costmap_, p, left_torelance,
+                              right_torelance, 0.1);
+    if (ret.modified) {
+      response->waypoints[i].pose = ret.pose;
+      response->modified = true;
+    }
+  }
+  if (response->modified && pose_) {
+    response->waypoints =
+        cut_behind(response->waypoints, pose_->pose.pose.position);
+  }
 }
 
 void WaypointObstacleAvoidanceNode::global_costmap_callback(
     const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
-  RCLCPP_INFO(this->get_logger(), "%d %d %lf %lf %lf", msg->info.width,
-              msg->info.height, msg->info.resolution,
-              msg->info.origin.position.x, msg->info.origin.position.y);
   global_costmap_ = msg;
+}
+
+void WaypointObstacleAvoidanceNode::pose_callback(
+    const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg) {
+  pose_ = msg;
 }
 
 } // namespace penguin_nav
